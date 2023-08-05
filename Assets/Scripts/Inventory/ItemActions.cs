58426bf89329733d1a6637e3526e4b43048ed5d2
Robby Klein <hello@robbyk.com>
2023-08-05 16:06:57 -0700

    start actions

---
+++ Assets/Scripts/Inventory/ItemActions.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class ItemActions : MonoBehaviour
+{
+    public static void UsePotion()
+    {
+        Debug.Log("Potion used.");
+        // Add code here to execute when a Potion is used
+    }
+
+    public static void UseAntidote()
+    {
+        Debug.Log("Antidote used.");
+    }
+}
