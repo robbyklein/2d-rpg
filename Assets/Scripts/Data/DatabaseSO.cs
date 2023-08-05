519105dded68d958f6f724b7a554ff7f80bfa8fd
Robby Klein <hello@robbyk.com>
2023-08-05 09:50:04 -0700

    remove old files

--- Assets/Scripts/Data/DatabaseSO.cs
+++ Assets/Scripts/Data/DatabaseSO.cs
@@ -3,47 +3,41 @@ using System.IO;
 using UnityEngine;
 
 [System.Serializable]
-public class PlayerData
-{
+public class PlayerData {
     public float Level = 1f;
     public float Health = 100f;
     public float MaxHealth = 100f;
     public float Defense = 10f;
 }
 
+
 [System.Serializable]
-public class Data
-{
+public class Data {
     public PlayerData PlayerData = new PlayerData();
 }
 
 
 [CreateAssetMenu(fileName = "Database", menuName = "ScriptableObjects/Data/Database")]
-public class DatabaseSO : ScriptableObject
-{
+public class DatabaseSO : ScriptableObject {
     private string filepath;
     public Data Data { get; private set; }
 
     public Action OnPlayerDataUpdate;
 
-    private void OnEnable()
-    {
+    private void OnEnable() {
         filepath ??= Path.Combine(Application.persistentDataPath, "savefile.json");
 
-        if (!File.Exists(filepath))
-        {
+        if (!File.Exists(filepath)) {
             CreateSavefile();
             SaveData();
         }
-        else
-        {
+        else {
             LoadData();
         }
     }
 
     #region Updaters
-    public void UpdateCharacterData(PlayerData newCharacterData)
-    {
+    public void UpdateCharacterData(PlayerData newCharacterData) {
         Data.PlayerData = newCharacterData;
         OnPlayerDataUpdate?.Invoke();
         SaveData();
@@ -51,20 +45,17 @@ public class DatabaseSO : ScriptableObject
     #endregion
 
     #region Create/Load/Save
-    private void CreateSavefile()
-    {
+    private void CreateSavefile() {
         Data data = new Data();
         SaveData();
     }
 
-    private void SaveData()
-    {
+    private void SaveData() {
         string json = JsonUtility.ToJson(Data);
         File.WriteAllText(filepath, json);
     }
 
-    private void LoadData()
-    {
+    private void LoadData() {
         string json = File.ReadAllText(filepath);
         Data = JsonUtility.FromJson<Data>(json);
     }
--- Assets/Scripts/Interfaces/IItem.cs
+++
@@ -1,5 +0,0 @@
-public interface IItem {
-    string Name { get; }
-
-    void OnUseItem();
-}
